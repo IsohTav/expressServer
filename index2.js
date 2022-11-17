fec8522b928c165d79f3d8de2248bd4a318af3aa
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 19:56:32 +1100

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const puppeteer = require('puppeteer');
+const server = express();
+
+const PORT = 8000;
+
+server.use(bodyParser.text()) 
+
+server.get('/', (req, res) => {
+	res.json({hello: "world"});
+
+});
+
+
+
+
+async function scrapeItem(url) {
+	
+	const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	const [el] = await page.$x('/html/body/section[2]/div/div[1]/div[1]/dl/dd/span');
+	const text = await el.getProperty('textContent');
+	const textTXT = await text.jsonValue();
+
+	return textTXT;
+}
+
+
+const data1 = [];
+
+server.post('/api/data1', (req, res) => {
+	    
+	    const data1info = req.body;
+	    data1.push(data1info);
+	    const scrapedData = scrapeItem(data1);
+
+	    res.status(201).send(scrapedData);
+		
+
+
+	    });
+
+server.listen(8000, () => {
+	console.log('incoming')
+
+});
