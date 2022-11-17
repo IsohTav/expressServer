4944d9b108a8f407defbf28a87c653a54b9195ee
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 20:45:22 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-
+const puppeteer = require('puppeteer');
 const server = express();
 
 const PORT = 8000;
@@ -13,16 +13,38 @@ server.get('/', (req, res) => {
 });
 
 
-const data1 = [];
+
+
+async function scrapeItem(url) {
+	
+	const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	const [el] = await page.$x('/html/body/section[2]/div/div[1]/div[1]/dl/dd/span');
+	const text = await el.getProperty('textContent');
+	const textTXT = await text.jsonValue();
+
+	return textTXT;
+}
+
+const data1info = '';
+
 
 server.post('/api/data1', (req, res) => {
 	    
-	    const data1info = req.body;
-	    data1.push(data1info);
-		console.log(data1info);
-		console.log(data1);
+	    const data1info = JSON.stringify(req.body);
 	    res.status(201).send(data1info);
-		
+		console.log(data1info);
+
+
+	    });
+
+server.post('/api/scrape1', async (req, res) => {
+	    
+	    const scrapedData = await scrapeItem(req.body);
+		res.send(scrapedData);
+		console.log(scrapedData);
 
 
 	    });
