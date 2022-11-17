1547d351e9a1baf19d3b6820e5200c365e8f0b72
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 19:09:35 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const server = express();
 
 const PORT = 8000;
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
 	res.json({hello: "world"});
 
 });
 
+
+const data1 = [];
+
 server.post('/api/data1', (req, res) => {
 	    
-	    const data1 = req.body;
-		res.json(data1)
-	    
+	    const data1info = req.body;
+	    data1info.id = shortid.generate();
+	    data1.push(data1info);
+	    res.status(201).json(data1info);
+		
+
+
 	    });
 
 server.listen(8000, () => {
