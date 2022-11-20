e3926239ef518e4ed8c0969ad633b65688f91567
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-20 13:14:54 +1100

    Update skillScraper.js

--- skillScraper.js
+++ skillScraper.js
@@ -279,7 +279,7 @@ async function scrapeIQ(url) {
 	IQ = await page.$$eval('dl.jobseek-dl.d-flex.flex-row.fs-16', elements2 => elements2.map(LL2 => {
 
 	        //Top skills section of OLJ profile
-	        const IQ2= LL2.querySelector('dd a').innerText;
+	        const IQ2= LL2.querySelector('dd').innerText;
 	       
 
 
