afb4dcf11ed214bcbccc2405716423a2eeccadc8
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-20 15:36:48 +1100

    Update scrapehub2.js

--- scrapehub2.js
+++ scrapehub2.js
@@ -314,6 +314,7 @@ server.post('/scraping/message', (req,res) => {
 const contact_email = req.body.contact_email
 	const subject = req.body.subject
 	const message1 = req.body.message1
+    const url3 = req.body.url2
 
 	fetch("https://www.onlinejobs.ph/contact/", {
   "headers": {
@@ -328,7 +329,7 @@ const contact_email = req.body.contact_email
     "sec-fetch-site": "same-origin",
     "x-requested-with": "XMLHttpRequest",
     "cookie": "hblid=QtRO5dBbzhrD0T8H7n9LP0NF6reDAB0A; _fbp=fb.1.1662388881877.1919595958; olfsk=olfsk02491868843945544; _jsuid=2910189676; _gid=GA1.2.1541528761.1668593420; _okdetect=%7B%22token%22%3A%2216686754921510%22%2C%22proto%22%3A%22about%3A%22%2C%22host%22%3A%22%22%7D; _ok=5952-252-10-9497; ci_session=pro76u90esl9dd0qqkpkeaa8hk6pn72l; wcsid=P3QpSq2BFRKCVLo17n9LP0EDarb0FoA8; _okbk=cd5%3Davailable%2Ccd4%3Dtrue%2Cvi5%3D0%2Cvi4%3D1668730803769%2Cvi3%3Dactive%2Cvi2%3Dfalse%2Cvi1%3Dfalse%2Ccd8%3Dchat%2Ccd6%3D0%2Ccd3%3Dfalse%2Ccd2%3D0%2Ccd1%3D0%2C; _ga_2ZN6B06KK8=GS1.1.1668730803.21.1.1668730903.30.0.0; _ga=GA1.1.1687723548.1662388881; mp_52e5e0805583e8a410f1ed50d8e0c049_mixpanel=%7B%22distinct_id%22%3A%20%22184404325e1cc0-0829f9bb5a1227-19525635-16a7f0-184404325e2129f%22%2C%22%24device_id%22%3A%20%22184404325e1cc0-0829f9bb5a1227-19525635-16a7f0-184404325e2129f%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D; _oklv=1668738214457%2CP3QpSq2BFRKCVLo17n9LP0EDarb0FoA8",
-    "Referer": "https://www.onlinejobs.ph/jobseekers/info/1457271",
+    "Referer": `${url3}`,
     "Referrer-Policy": "strict-origin-when-cross-origin"
   },
   "body": `csrf-token=88e4f55b16284a5a93a90a108d2c8503&contact_email=${contact_email}&back_id=1457271&job_id=0&op=1&from_modal=1&info%5Bname%5D=Steven+Greffe&info%5Bemail%5D=admin%40findmyva.com.au&info%5Bsubject%5D=${subject}&Template=&info%5Bmessage%5D=${message1}`,
