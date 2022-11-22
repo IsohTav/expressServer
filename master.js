f54795d4bb892b39a3739a69d4ce2144f16c5090
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-22 21:46:04 +1100

    Update master.js

--- master.js
+++ master.js
@@ -94,7 +94,7 @@
 
 	server.post('/test1', async (req, res) => {
 
-		const jobTD = req.post.jobTD;
+		const jobTD = req.body.jobTD;
 		const threadPage1 = await oljThreadMAX(jobTD);
 		const tp2 = await threadPage1.length;
 		const tp3 = await Math.round(tp2 / 10);
