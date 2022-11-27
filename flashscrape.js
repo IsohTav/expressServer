1df0b1f8cd948645a38ecaf5726f7c1533fcda87
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-27 21:02:21 +1100

    Update flashscrape.js

--- flashscrape.js
+++ flashscrape.js
@@ -29,9 +29,10 @@
 
 	fixtures = await page.$$eval('div.sportName.table-tennis div', elements => elements.map(LL => LL.id));
 
+	const ids = fixtures.map(o => o.id)
+	const filtered = fixtures.filter(({id}, index) => !ids.includes(id, index + 1))
 
-
-	console.log(fixtures);
+	console.log(filtered);
 	return fixtures;
 
 		};
