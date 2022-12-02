0e99b60f13ca19e29b78e1825b5091dcf85c388a
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-02 16:02:00 +1100

    Update balanceping.js

--- balanceping.js
+++ balanceping.js
@@ -32,10 +32,11 @@ const express = require('express');
 
 	async function getRecordID(searchID) {
 
-		const ID = await fetch(`https://api.airtable.com/v0/appkdHxBh7f5oCzwZ/VMs?filterByFormula=search(${searchID}%2C%7Bvm+ID%7D)`, {
+		const response = await fetch(`https://api.airtable.com/v0/appkdHxBh7f5oCzwZ/VMs?filterByFormula=search(${searchID}%2C%7Bvm+ID%7D)`, {
 			"headers": {
 				"Authorization": "Bearer key8q2CivSfd21Mpu"}});
 
+		const ID = await response.json();
 		return ID;
 
 
