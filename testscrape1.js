39cddaac9746afe1574a8b9dfb53ff14a7813d18
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-06 21:51:21 +1100

    Update testscrape1.js

--- testscrape1.js
+++ testscrape1.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://www.flashscore.com/table-tennis/others-men/liga-pro-cz/results/');
   await page.screenshot({path: 'example.png'});
