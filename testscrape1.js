2d83954dc38da7127b5de46cf87f770f61234986
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-07 10:38:00 +1100

    Update testscrape1.js

--- testscrape1.js
+++ testscrape1.js
@@ -5,6 +5,8 @@ const puppeteer = require('puppeteer');
   await page.goto('https://www.flashscore.com/table-tennis/others-men/liga-pro-cz/results/');
   await page.screenshot({path: 'example.png'});
   const divs = await page.$$eval('div.event.event--results div div.sportName.table-tennis div', divs => divs.map(div => div.id));
+  const filteredDivs = divs.filter(Boolean);
   console.log(divs);
+  console.log(filteredDivs);
   await browser.close();
 })();
