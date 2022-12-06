933b43fd08bb5e249a431c5e894ce3591cc3fc36
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-06 21:50:37 +1100

    Update testscrape1.js

--- testscrape1.js
+++ testscrape1.js
@@ -1,24 +1,10 @@
 const puppeteer = require('puppeteer');
 (async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.flashscore.com/table-tennis/others-men/liga-pro-cz/results/');
   await page.screenshot({path: 'example.png'});
-  const results = await page.$$eval('#fs-results div.fs-table div.fs-table__body div.fs-table__row', rows => {
-    return rows.map(row => {
-      const columns = row.querySelectorAll('div.fs-table__cell');
-      return {
-        date: columns[0].innerText,
-        time: columns[1].innerText,
-        homeTeam: columns[2].innerText,
-        awayTeam: columns[3].innerText,
-        score: columns[4].innerText,
-        status: columns[5].innerText,
-        link: columns[2].querySelector('a').href,
-      };
-    });
-  });
-  const mozolGames = results.filter(result => result.homeTeam.includes('Mozol P.') || result.awayTeam.includes('Mozol P.'));
-  console.log(mozolGames);
+  const divs = await page.$$eval('#live-table > div.event.event--results > div > div', divs => divs.map(div => div.id));
+  console.log(divs);
   await browser.close();
 })();
