a5262905abaebe6adf8de05bb7d79ffc60ec5b6c
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-08 17:27:11 +1100

    Update wise2.js

--- wise2.js
+++ wise2.js
@@ -1,35 +1,33 @@
 /* require axios and puppeteer */
-var axios = require('axios');
-var puppeteer = require('puppeteer');
-var bodyParser = require('body-parser');
-
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
+const axios = require("axios");
+const puppeteer = require("puppeteer");
+const bodyParser = require("body-parser");
 
 /* set token */
 const api = "2594b963-9cfb-40ce-82d2-8cd85197fc0a";
 
-/*return wise profileID for business account*/
+/* return wise profileID for business account */
 async function getProfile(api) {
-  let profiles = await axios.get('https://api.sandbox.transferwise.tech/v1/profiles', { headers: { 'Authorization': `Bearer ${api}` } }).then(response => {
-    return response.data.filter(profile => profile.type === 'business')[0].id;
-  });
+  let profiles = await axios
+    .get("https://api.sandbox.transferwise.tech/v1/profiles", {
+      headers: { Authorization: `Bearer ${api}` },
+    })
+    .then((response) => {
+      return response.data.filter((profile) => profile.type === "business")[0].id;
+    });
   console.log(profiles);
   return profiles;
 }
 
 /* create a quote in wise and return the id */
-
 const https = require("https");
 
 function createQuote(profile, sourceCurrency, targetCurrency, targetAmount) {
-  const data = {
-          "profileId":`${profile}`,
-          "sourceCurrency": `${sourceCurrency}`,
-          "targetCurrency": `${targetCurrency}`,
-          "sourceAmount": `${targetAmount}`,
-         
-        }
+  const data = JSON.stringify({
+    profileId: profile,
+    sourceCurrency: sourceCurrency,
+    targetCurrency: targetCurrency,
+    sourceAmount: targetAmount,
   });
 
   const options = {
@@ -37,7 +35,7 @@ function createQuote(profile, sourceCurrency, targetCurrency, targetAmount) {
     path: `/v3/profiles/${profile}/quotes`,
     method: "POST",
     headers: {
-      "Authorization": "Bearer 2594b963-9cfb-40ce-82d2-8cd85197fc0a",
+      Authorization: "Bearer 2594b963-9cfb-40ce-82d2-8cd85197fc0a",
       "Content-Type": "application/json",
       "Content-Length": data.length,
     },
@@ -45,19 +43,4 @@ function createQuote(profile, sourceCurrency, targetCurrency, targetAmount) {
 
   const req = https.request(options, (res) => {
     // handle the response from the server here
-    res.on("data", (d) => {
-      const responseData = JSON.parse(d.toString());  // <-- convert the Buffer to a string and parse the JSON
-      console.log(responseData);  // <-- log the response data to the console
-    });
-  });
-
-  req.write(data);
-  req.end();
-  req.on("error", (error) => {
-    console.error(error);
-  });
-}
-
-// example usage
-createQuote(16622021, "AUD", "AUD", 100);  // <-- updated arguments to create a quote for AUD to AUD
-
+    res.on("data", (
