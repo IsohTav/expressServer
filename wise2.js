e10e5631c4ca962300e93e2231893362c9e71c6e
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-08 15:59:52 +1100

    Update wise2.js

--- wise2.js
+++ wise2.js
@@ -2,17 +2,15 @@
 var axios = require('axios');
 var puppeteer = require('puppeteer');
 const api = "2594b963-9cfb-40ce-82d2-8cd85197fc0a";
-/* create getProfile function */
 async function getProfile(api) {
-  let profiles = await axios.get('https://api.sandbox.transferwise.tech/v1/profiles', { headers: { 'Authorization': `Bearer ${api}` } }).then(response => { return (response.data) });
-  console.log (profiles);
+  let profiles = await axios.get('https://api.sandbox.transferwise.tech/v1/profiles', { headers: { 'Authorization': `Bearer ${api}` } }).then(response => {
+    return response.data.filter(profile => profile.type === 'business')[0].id;
+  });
+  console.log(profiles);
   return profiles;
 }
-
-const profileId = getProfile(api).then(profiles => {
-  return profiles.filter(profile => {
-    return profile.type === 'business';
-  })[0].id;
-});
-
-console.log(profileId);
+async {
+await getProfiles(api);
+  
+  
+};
