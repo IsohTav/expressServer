e6d223224d95cab551d9bbc8ce9a87219771fb70
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-12-08 18:02:29 +1100

    Update wise2.js

--- wise2.js
+++ wise2.js
@@ -24,48 +24,38 @@ async function getProfile(api) {
 
 
 function createQuote(profile, sourceCurrency, targetCurrency, targetAmount) {
-  const data = {
-  "profileId": `${profile}`,
-  "sourceCurrency": `${sourceCurrency}`,
-  "targetCurrency": `${targetCurrency}`,
-  "sourceAmount": `${targetAmount}`,
-}; 
+  const data = JSON.stringify({
+          "profileId":`${profile}`,
+          "sourceCurrency": `${sourceCurrency}`,
+          "targetCurrency": `${targetCurrency}`,
+          "sourceAmount": `${targetAmount}`,
+        });
 
   const options = {
     hostname: "api.sandbox.transferwise.tech",
     path: `/v3/profiles/${profile}/quotes`,
     method: "POST",
     headers: {
-      Authorization: "Bearer 2594b963-9cfb-40ce-82d2-8cd85197fc0a",
+      "Authorization": "Bearer 2594b963-9cfb-40ce-82d2-8cd85197fc0a",
       "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(data),
+      "Content-Length": data.length,
     },
   };
 
   const req = https.request(options, (res) => {
-    // Set the encoding for the data that is received from the server
-    res.setEncoding("utf8");
-
-    // Handle the response from the server here
+    // handle the response from the server here
     res.on("data", (d) => {
-      // Use the try/catch statement to catch any errors that occur when parsing the response data
-      try {
-        const responseData = JSON.parse(d.toString());  // <-- convert the Buffer to a string and parse the JSON
-
-        // Map the id of the quote to a constant using the Object.assign() method
-        const quote = Object.assign({}, { id: responseData.id });
-        console.log(quote);  // <-- log the quote object to the console
-      } catch (error) {
-        console.error(error);  // <-- handle any errors that occur when parsing the response data
-      }
+      const responseData = JSON.parse(d.toString());  // <-- convert the Buffer to a string and parse the JSON
+      console.log(responseData);  // <-- log the response data to the console
     });
   });
 
   req.write(data);
-  req.end();  // <-- signal the end of the request
+  req.end();
   req.on("error", (error) => {
-    console.error(error);  // <-- handle any errors that occur during the request
+    console.error(error);
   });
 }
-// example usage
-createQuote(16622021, "AUD", "AUD", 100);  // <-- updated arguments to create a quote for AUD to AUD
+const quoteData = '{"sourceCurrency": "GBP", "targetCurrency": "USD", "sourceAmount": 100}';
+const quote = JSON.parse(quoteData);
+createQuote(quote);
